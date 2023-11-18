21bfe3b496b7d325f8b3bf523fb526e5f44a1cf7
Sinzxc <nik.orig@mail.ru>
2023-11-18 12:09:36 +0300

    add

---
+++ app/src/main/java/ru/mggtk/practice22_fix_curiosity/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.mggtk.practice22_fix_curiosity
+
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle? ){
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
+
